3648df4d8be9461c8fe6deb21e6b7815ba49a6a2
Sambld <samibld18@gmail.com>
2021-06-11 17:54:43 +0100

    add patient consultations

--- src/views/Patient.vue
+++ src/views/Patient.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <v-container fluid>
+      <v-progress-linear
+          indeterminate
+          color="green lighten-2"
+          v-if="loadingContents"
+      ></v-progress-linear>
       <v-subheader>
         <router-link :to="`/dashboard`" style="text-decoration: none;    ;">
           <v-icon :color="`${(mode)?'#017e5e':'primary darken-4'}`" class="mr-2">
@@ -13,10 +18,10 @@
         </span>
         </router-link>
         <span>
-          / Patient
+          / Patient / {{ patientInfo.firstname }}  {{ patientInfo.lastname }}
         </span>
       </v-subheader>
-      <v-tabs centered grow>
+      <v-tabs v-if="!loadingContents" centered grow>
         <v-tabs-slider></v-tabs-slider>
         <v-tab>info</v-tab>
         <v-tab-item>
@@ -157,7 +162,8 @@
               <v-data-table
                   :headers="appointments.headers"
                   :items="appointments.items"
-                  :items-per-page="5"
+                  hide-default-footer
+
                   class="elevation-1"
               >
                 <template v-slot:item.state="{ item }">
@@ -192,6 +198,48 @@
           </v-row>
 
         </v-tab-item>
+
+
+        <v-tab>Consultation</v-tab>
+        <v-tab-item>
+          <div class="text-center py-5" v-if="consultations.items.length == 0">No appointments yet</div>
+          <v-data-table
+              :headers="consultations.headers"
+              :items="consultations.items"
+              hide-default-footer
+
+              class="elevation-1 my-5"
+          >
+            <template v-slot:item.syndromes="{ item }">
+              <v-chip
+                  color="red"
+                  dark
+              >
+                {{ item.syndroms }}
+              </v-chip>
+            </template>
+            <template v-slot:item.type="{ item }">
+              <v-chip
+                  :color="getColor(item.type)"
+                  dark
+              >
+                {{ item.type }}
+              </v-chip>
+            </template>
+            <template v-slot:item.details="{ item }">
+              <v-btn
+                  :to="`/consultations/${item.details}`"
+                  outlined
+                  :color="(mode)?'secondary darken-2':'primary darken-2'"
+              >
+                <v-icon :color="(mode)?'secondary darken-2':'primary darken-2'">
+                  mdi-open-in-new
+                </v-icon>
+              </v-btn>
+            </template>
+          </v-data-table>
+        </v-tab-item>
+
       </v-tabs>
 
 
@@ -276,7 +324,9 @@
           :scrollable="false"
           @click:outside="closeOverLay(true)"
       >
-        <AddAppointment v-if="hover" @ShowSnackBar="showSnackBar(message,color)" :HideOverLay="closeOverLay" :dateApp="dateApp" :timeApp="timeApp" :timeLApp="timeLApp" :revisitApp="revisit" :patientId="patientInfo.id" :appointmentId="``" :color="color"/>
+        <AddAppointment v-if="hover" @ShowSnackBar="showSnackBar(message,color)" :HideOverLay="closeOverLay"
+                        :dateApp="dateApp" :timeApp="timeApp" :timeLApp="timeLApp" :revisitApp="revisit"
+                        :patientId="patientInfo.id" :appointmentId="``" :color="color"/>
       </v-dialog>
 
     </div>
@@ -344,9 +394,11 @@ export default {
     'mode'
   ],
   data: () => ({
+    loadingContents: true,
     patientId: null,
     patientInfo: {
-      appointments: []
+      appointments: [],
+      consultations: []
     },
     errors: {},
     editPatientDialog: false,
@@ -354,13 +406,13 @@ export default {
 
     snackbar: false,
     snackbarApp: false,
-    hover : false,
-    revisit : true,
+    hover: false,
+    revisit: true,
     snackbarMessage: '',
-    dateApp : '',
-    timeApp : '08:00',
-    timeLApp : 15,
-    color : 'teal darken-1',
+    dateApp: '',
+    timeApp: '08:00',
+    timeLApp: 15,
+    color: 'teal darken-1',
     snackbarColor: '',
     message: '',
 
@@ -369,7 +421,7 @@ export default {
         {
           text: 'Date',
           align: 'center',
-          sortable: true,
+          sortable: false,
           value: 'date',
         },
         {
@@ -403,6 +455,37 @@ export default {
           text: 'Link',
           sortable: false,
           value: 'link',
+          align: 'center',
+        },
+      ],
+      items: []
+    },
+
+    consultations: {
+      headers: [
+        {
+          text: 'Date & Time',
+          align: 'center',
+          sortable: false,
+          value: 'date',
+        },
+        {
+          align: 'center',
+          text: 'Syndromes',
+          sortable: false,
+          value: 'syndromes',
+        },
+        {
+          text: 'type',
+          sortable: false,
+          value: 'type',
+          align: 'center',
+
+        },
+        {
+          text: 'Details',
+          sortable: false,
+          value: 'details',
           align: 'center',
         },
       ],
@@ -443,7 +526,7 @@ export default {
             console.log('deleting user ' + this.patientInfo.id);
             this.deletePatientDialog = false
 
-            this.showSnackBar('Patient Deleted' , 'red')
+            this.showSnackBar('Patient Deleted', 'red')
             this.$router.push({name: 'patients'})
 
           })
@@ -452,6 +535,7 @@ export default {
           })
     },
     getPatientInfo() {
+
       this.axios.get(`/patient/${this.patientId}?with_appointments=1`).then(res => {
         this.appointments.items = []
         this.patientInfo = res.data
@@ -466,7 +550,20 @@ export default {
           })
         })
         this.appointments.items = this.orderly(this.appointments.items)
+
+        this.patientInfo.consultations.forEach(item => {
+          this.consultations.items.push({
+            date : `${item.created_at.slice(0,10)}   ${item.created_at.slice(11,19)} `,
+            syndroms : item.motive.split(',')[0],
+            type : item.type ,
+            details: item.id
+          })
+        })
+        console.log(this.consultations.items)
+
+        this.loadingContents = false
       }).catch(err => {
+        console.log(err)
         this.errors = err.response.data
         console.log(err.response)
 
@@ -474,22 +571,21 @@ export default {
     },
     getColor(state) {
 
-      if( state == 'consult'){
+      if (state == 'consult' || state == 'check') {
         return 'blue'
-      }else if (state == 'revisit'){
+      } else if (state == 'revisit') {
         return 'teal'
       }
 
-      if (state == 'waiting') {
+      if (state == 'waiting' || state == 'surgery') {
         return 'orange'
-      } else if (state == 'missed') {
+      } else if (state == 'missed' ) {
         return 'red'
-      }
-      else{
+      } else {
         return 'green'
       }
     },
-    CoverterSimpleDate(sdate){
+    CoverterSimpleDate(sdate) {
       var date = new Date();
       var FDArray = sdate.split(' ')
       var DateArray = FDArray[0].split('-')
@@ -502,16 +598,16 @@ export default {
       date.setSeconds(TimeArray[2])
       return date;
     },
-    orderly(list){
+    orderly(list) {
       var NoOrdredList = list
-      NoOrdredList.sort(function(a, b) {
+      NoOrdredList.sort(function (a, b) {
         const first_start = a.date.split('-').join('')
         const second_start = b.date.split('-').join('')
         return first_start - second_start;
       });
       return NoOrdredList;
     },
-    addAppointment (){
+    addAppointment() {
       var currectTime = new Date()
       var dateApp = currectTime.getFullYear() + '-' + (currectTime.getMonth() + 1) + '-' + currectTime.getDate();
 
@@ -522,12 +618,13 @@ export default {
       this.hover = true
     },
 
-    closeOverLay(){
+    closeOverLay() {
       this.hover = false
       this.getPatientInfo()
     },
   },
   created() {
+
     this.patientId = this.$route.params.id
     this.getPatientInfo()
   }
